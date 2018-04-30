938d6dc9a2739003ac8d11d4004ad7011a7bdaaf
Hai Bacti <haibacti@MacCandy.local>
2018-04-30 11:40:47 +0700

    +

--- src/demo_create_mongo_db.js
+++ src/demo_create_mongo_db.js
@@ -4,12 +4,12 @@ var url = 'mongodb://localhost:27017/mydb'
 MongoClient.connect(url, (err, db) =>
 {
     if (err) throw err
-    console.log('Database created!')
     var dbo = db.db('mydb')
-    dbo.createCollection('customers', function(err, res)
+    var myobj = { name: 'Company Inc', address: 'Highway 37' }
+    dbo.collection('customers').insertOne(myobj, (err, res) =>
     {
-        if (err) throw err
-        console.log('Collection created!')
+        if (err) throw err;
+        console.log('1 document inserted')
         db.close()
-    })
+    });
 })
