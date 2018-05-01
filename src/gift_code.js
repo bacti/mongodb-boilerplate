d9dc69600958f8b234d311b4d8b24af991fee792
Hai Bacti <haibacti@MacCandy.local>
2018-05-01 22:40:05 +0700

    +

--- src/gift_code.js
+++ src/gift_code.js
@@ -1,22 +1,49 @@
-// https://stackoverflow.com/questions/1073956/how-to-generate-63-million-prize-codes
+var MongoClient = require('mongodb').MongoClient
+var url = 'mongodb://localhost:27017/mydb'
 
 const alfabet = '0123456789ABDEFGHJKLMNPRSTUVXYZ'
 let GiftCode =
 {
+    // https://stackoverflow.com/questions/1073956/how-to-generate-63-million-prize-codes
     get random()
     {
         let code = ''
         ;[...Array(5)].map( _ =>
         {
-            code += alfabet[Math.floor(Math.random() * 32)]
+            code += alfabet[Math.floor(Math.random() * 31)]
         })
         code += '-'
         ;[...Array(5)].map( _ =>
         {
-            code += alfabet[Math.floor(Math.random() * 32)]
+            code += alfabet[Math.floor(Math.random() * 31)]
         })
         return code    
     }
 }
 
-console.log(GiftCode.random)
+MongoClient.connect(url, (err, db) =>
+{
+    if (err) throw err
+    var dbo = db.db('mydb')
+
+    // dbo.collection('gift').insertOne({ _id: GiftCode.random }, (err, res) =>
+    // {
+    //     if (err) throw err
+    //     console.log(res)
+    //     db.close()
+    // })
+
+    dbo.collection('gift').find({},).toArray((err, result) =>
+    {
+        if (err) throw err
+        console.log(result)
+        db.close()
+    })
+
+    // dbo.collection('gift').drop((err, delOK) =>
+    // {
+    //     if (err) throw err
+    //     if (delOK) console.log('Collection deleted')
+    //     db.close()
+    // })
+})
