1779dc91b5090bab3ab078477078ca0b3f65abb9
bacti <tranmhaii@gmail.com>
2018-05-02 09:45:25 +0700

    +

--- src/qrcode.js
+++ src/qrcode.js
@@ -1,5 +1,13 @@
 var QRCode = require('qrcode')
 
-QRCode.toDataURL('I am a pony!', function (err, url) {
-  console.log(url)
+QRCode.toDataURL('I am a pony!', function (err, url)
+{
+    console.log(url)
+
+    var base64Data = url.replace(/^data:image\/png;base64,/, "")
+
+    require("fs").writeFile("out.png", base64Data, 'base64', function(err)
+    {
+        console.log(err);
+    });
 })
